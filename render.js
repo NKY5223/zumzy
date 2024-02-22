efd032e3cbd4a9978594b150544b54015e441aad
AdderallSnorter <159595902+AdderallSnorter@users.noreply.github.com>
2024-02-22 12:34:05 -0600

    fixing pink mode colors

--- render.js
+++ render.js
@@ -171,8 +171,8 @@ function render() {
     }
     
     if (pinkMode) {
-        map.color = "rgb(76, 51, 51)";
-        map.background = "rgb(65, 32, 45)";
+        map.color = "rgb(243, 92, 230)";
+        map.background = "rgb(139, 63, 94)";
     }
 
     ctx.fillStyle = map.color;
